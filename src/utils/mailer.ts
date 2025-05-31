be58b7dbea6d573793ef9c8b00fb7b6b3921b5be
Su-007-ux <jesusdobles007@gmail.com>
2025-05-31 17:03:25 -0400

    mail

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -6,7 +6,7 @@ import nodemailer from 'nodemailer';
  */
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
+  port: Number(process.env.EMAIL_PORT), // 587
   secure: false,
   auth: {
     user: process.env.EMAIL_USER,
