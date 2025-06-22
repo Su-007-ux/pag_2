2a2f61cdd9188cadcc868742b1647f4a9c922f84
Su-007-ux <jesusdobles007@gmail.com>
2025-06-21 20:28:10 -0400

    idk4

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,3 +4,11 @@ export function requireLogin(req: Request, res: Response, next: NextFunction) {
   if (req.session && req.session.userId) return next();
   res.redirect('/login');
 }
+
+export function requireAdmin(req: Request, res: Response, next: NextFunction) {
+  console.log('Session en dashboard:', req.session);
+  if (req.session && req.session.userId === 'admin' && req.session.isAdmin) {
+    return next();
+  }
+  return res.redirect('/');
+}
