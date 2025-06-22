756ff0b8c74146fea623c7fc358922ebe6725345
Su-007-ux <jesusdobles007@gmail.com>
2025-06-21 22:53:05 -0400

    idk6

--- src/routes/dashboardRoutes.ts
+++ src/routes/dashboardRoutes.ts
@@ -4,9 +4,8 @@ const router = Router();
 
 // Middleware para proteger la ruta solo para admin
 function requireAdmin(req: Request, res: Response, next: NextFunction) {
-  console.log('Session en dashboard:');
-  if (req.session.userId === 'admin' && req.session.isAdmin === true) {
-    console.log('Session en dashboard:');
+  if (req.session.userId === 'admin') {
+    console.log('Session en dashboard');
     return next();
   }
   return res.redirect('/');
