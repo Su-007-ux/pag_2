a32bc3a2bed00b4e6451fc1936530ddc59cac94b
Su-007-ux <jesusdobles007@gmail.com>
2025-06-27 16:57:02 -0400

    eu1

--- src/types/express-session/index.d.ts
+++ src/types/express-session/index.d.ts
@@ -2,9 +2,9 @@ import 'express-session';
 
 declare module 'express-session' {
   interface SessionData {
-    lang?: string;
-    userId?: string | number;
+    userId?: number | string;
     isAdmin?: boolean;
     loginSuccess?: boolean;
+    lang?: string;
   }
 }
